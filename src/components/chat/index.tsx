a996f82f3a54d609e66e6cfdd8858cfe7446dccd
iam-kevin <kevin.al.james@gmail.com>
2021-02-24 01:49:32 +0300

    added example contents to the page

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,7 +1,14 @@
+import { Potato } from '../react-chat-potato/@types';
 import { PotatoChat } from '../react-chat-potato/src'
 import { ImageComposer, TextComposer } from '../react-chat-potato/src/lib';
 
-const globalContextState = {
+
+interface User {
+    name: string
+    avatar?: string
+}
+
+const globalChatContext: Potato.GlobalChatContext<User> = {
     dateTime: Date.now(),
     users: {
         'self': null,   // TODO: this should indicate that user can chat
@@ -14,7 +21,7 @@ const globalContextState = {
     }
 }
 
-const messages = [
+const messages: Potato.Messages<MessageInputType> = [
     {
         input: "Hi here, how are you doing", 
         dateTimeDelta: 129122762,
@@ -26,48 +33,49 @@ const messages = [
         user: 'brian'
     }
 ]
+
 /**
  * Composer Component details
  */
-// interface Composer<T> {
-//     component: (props: ComposerComponentProps<T>) => JSX.Element
-// }
+type ComposerType =
+    | 'text'
+    | 'image'
 
-// type ComposerMap = { [type in ComposerType]: Atom<Composer<ComposerMessageInputType>> }
+type MessageInputType =
+    | string    // message type for text
+    | string    // message type for image
 
 
-const composerOptions = {
+const composerOptions: Potato.Composer.GlobalContext<ComposerType, MessageInputType> = {
     composerType: 'text',    // default composer 'text' | options 'text', 'image',
-    sendAction: async (input: any) => {
-        return;
-    },
     // You might want to move this to the BaseComposer
     composerOptions: {
         'text': {
-            component: TextComposer
+            component: TextComposer,
         },
-        'image': { 
+        'image': {
             component: ImageComposer,
         },
     }
 }
 
+
 export default function ChatBox() {
-    const sendCallback = async (message: any) => {
-      console.log("Sent the message")
+    const sendAction = async (input: Potato.Composer.NewMessage<MessageInputType>, composerType: ComposerType) => {
+        console.log("Sent the message")
+        switch (composerType) {
+            case 'text': console.log("Text based message"); break;
+            case 'image': console.log("Image based message"); break;
+            default: console.log("DEFAULT MESSAGE TRANSFER")
+        }
     }
 
     return (
         <PotatoChat 
             initialComposer='text'
             initialMessages={messages}
-            globalChatContext={globalContextState}
+            globalChatContext={globalChatContext}
             composerOptions={composerOptions}
-            sendCallback={sendCallback} />
-        // <PotatoProvider initialMessages={[]}>
-        //     <PotatoChat
-        //         initialComposer='text'
-        //         sendCallback={sendCallback}/>
-        // </PotatoProvider>  
+            sendAction={sendAction} />
     )
 }
